72fdd47d6b9361a18346e78dec1d3565da310350
Alba Gómez Pérez <albagope@gmail.com>
2019-10-19 20:16:17 +0200

    improve name classes

--- src/CharacterList.js
+++ src/CharacterList.js
@@ -7,20 +7,20 @@ const CharacterList = props => {
     const {AllCharacters, SearchName} = props;
     // console.log(AllCharacters);
     return (
-        <ul className="cartoonsList" >
+        <ul className="pokemonList" >
             {AllCharacters
                 .filter(myCartoon => myCartoon.name.toUpperCase().includes(SearchName.toUpperCase()))
                 .map((item, index) => {
                     return (
-                        <li className="cartoon" key={index}>
+                        <li className="pokemon" key={index}>
                                 <img className="pokemonImage" src={item.sprites.front_default} alt={item.name}/>
                             <div className="infoContainer">
-                                <div className="cartoonName"> {item.name}</div>
-                                <div className="weight">{item.id}</div>
-                                <div className="types">
+                                <div className="pokemonName"> {item.name}</div>
+                                <div className="pokemonWeight">{item.id}</div>
+                                <div className="pokemonTypes">
                                     {item.types.map(typeInfo => {
                                         return (
-                                            <span className="type">{typeInfo.type.name}</span>
+                                            <span className="pokemonType">{typeInfo.type.name}</span>
                                         )
                                     })}
                             </div>
