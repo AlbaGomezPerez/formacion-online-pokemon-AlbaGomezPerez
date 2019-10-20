7be23669064920ca78db8daeffe5c3fd3f6b7bbf
Alba Gómez Pérez <albagope@gmail.com>
2019-10-20 19:43:28 +0200

    docs

---
+++ docs/precache-manifest.a30d3ec68d60c57c23ccce305f3920b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0193a39388fd5a3ded59db39d10630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454f53d5f182802066a7",
+    "url": "/static/css/main.be8b69e5.chunk.css"
+  },
+  {
+    "revision": "e7155c7f61bbcbb770dd",
+    "url": "/static/js/2.fa50aa46.chunk.js"
+  },
+  {
+    "revision": "454f53d5f182802066a7",
+    "url": "/static/js/main.3c86aa1c.chunk.js"
+  },
+  {
+    "revision": "9fa757ed08e73648c40f",
+    "url": "/static/js/runtime-main.e7ce8e0b.js"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "/static/media/pokemon.49255cb2.png"
+  }
+]);
